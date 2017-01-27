3dfec772d5c1668c7e6129931697aab54f1b4173
David G. Bethelmy <bethelmyd@cookman.edu>
2017-01-27 16:53:53 -0500

    typed up everything. Testing now

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
+var db = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
@@ -11,36 +11,36 @@ router.get("/", function(req, res) {
 });
 
 router.get("/burgers", function(req, res) {
-  burger.selectAll(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+    db.Burger.findAll({})
+    .then(function(dbBurger) {
+      res.json(dbBurger);
+      res.redirect("/burgers");
+    });
 });
 
 router.post("/burgers/create", function(req, res) {
-  burger.insertOne([
-    "burger_name"
-  ], [
-    req.body.burger_name
-  ], function() {
-    res.redirect("/burgers");
-  });
+    db.Burger.create({
+      burger_name: req.body.burger_name
+    })
+    .then(function(dbBurger) {
+      res.json(dbBurger);
+      res.redirect("/burgers");
+    });
 });
 
 router.put("/burgers/update/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
-
-  burger.updateOne({
-    devoured: req.body.devoured
-  }, condition, function() {
-    res.redirect("/burgers");
+    db.Burger.update(req.body,
+      {
+        where: {
+          id: req.params.id
+        }
+      })
+    .then(function(dbBurger) {
+      res.json(dbBurger);
+      res.redirect("/burgers");
+    });
   });
-});
 
 // Export routes for server.js to use.
 module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,13 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
+
 
 var app = express();
+
+// Requiring our models for syncing. This gets things from index.js
+var db = require("./models/index.js");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
@@ -25,4 +29,10 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.listen(port);
+// Syncing our sequelize models and then starting our express app
+//I took out force: true from within sync
+db.sequelize.sync({}).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
