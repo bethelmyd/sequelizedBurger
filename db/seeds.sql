69406969b78ea6087ded543f2f9aef0e21f95d3e
David G. Bethelmy <bethelmyd@cookman.edu>
2017-01-27 12:51:43 -0500

    Starting with burger

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, date) VALUES ('Big Mac', now());
+INSERT INTO burgers (burger_name, date) VALUES ('Whopper', now());
+INSERT INTO burgers (burger_name, date) VALUES ('Whopper Jr.', now());
